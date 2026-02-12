584e4b8c68c4eb4f9748e6010a8a543393f37e3c
yacktrd <trdyank@gmail.com>
2026-02-12 22:43:56 +0100

    fix async load

--- web/app.js
+++ web/app.js
@@ -1,9 +1,6 @@
 import { loadAssets } from "./engine.js";
 
-// AUTO LOAD ON START
-document.addEventListener("DOMContentLoaded", () => {
-    loadMarket();
-});
+async function init() {
 
   const assets = await loadAssets();
 
@@ -13,17 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   container.innerHTML = assets
     .slice(0, 50)
-    .map(
-      a => `
+    .map(a => `
       <div class="row">
         <span>${a.symbol}</span>
-        <span>$${a.price}</span>
+        <span>${a.price}</span>
         <span>${a.change24h.toFixed(2)}%</span>
         <span>${a.score}</span>
       </div>
-    `
-    )
+    `)
     .join("");
 }
 
-init();
+document.addEventListener("DOMContentLoaded", init);
