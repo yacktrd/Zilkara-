39dd4baa99315a874ecaa43bba25180ac1d21ff1
yacktrd <trdyank@gmail.com>
2026-02-14 22:13:18 +0100

    fix: remove index.html to enable nextjs runtime

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Zilkara",
+  description: "Zilkara Market Scanner"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
