8fdcd7d07e9eb28584eec06720d2deda8fe38c81
yacktrd <trdyank@gmail.com>
2026-02-15 07:09:28 +0100

    add zilkara homepage

--- rfs-crypto-v1/app/page.js
+++ rfs-crypto-v1/app/page.js
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
     <main style={{
-      background: "black",
-      color: "white",
+      background: "#0a0a0a",
+      color: "#fff",
       height: "100vh",
-      padding: "40px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
       fontFamily: "Arial"
     }}>
-      <h1>Zilkara</h1>
-      <p>Market Scanner operational</p>
+      <div>
+        <h1>Zilkara</h1>
+        <p>Market Scanner Core Online</p>
+      </div>
     </main>
   );
 }
